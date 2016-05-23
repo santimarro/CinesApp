a86b70513d42c31461e98d59a9e929232d6eccc6
santimarro <smarro@gmail.com>
2016-05-23 19:29:37 -0300

    Comienzo del repositorio con el trabajo ya realizado hasta hoy

---
+++ app/src/main/java/com/ophion/cinesapp/Schedules.java
@@ -0,0 +1,25 @@
+/* package com.ophion.cinesapp;
+
+import java.util.List;
+
+/**
+ * Created by smarro on 5/23/16.
+ */
+/*
+public class Schedules {
+    private List<String> schedule;
+
+    public Schedules(){
+    }
+    public Schedules(String hour) {
+        this.HoytsOlmos = [];
+        this.HoytsNvoCentro = [];
+        this.GranRex = [];
+        this.Cinerama = [];
+        this.Showcase = [];
+    }
+    public void setASchedule(String cine, List<String> schedule) {
+        this.cine = schedule;
+    }
+}
+*/
